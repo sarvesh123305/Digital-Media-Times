f2b8fc7eebd58458cf67a41c4108318522fb6ef4
sarvesh123305 <sarvesh123305@gmail.com>
2023-04-06 11:33:51 +0530

    UI created for news display

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -9,9 +9,9 @@ export class NewsItem extends Component {
         <div className="card " style={{width: '18rem'} }  >
   <img src={imageURL} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title} </h5>
-    <p className="card-text">{description}</p>
-    <a href="/newsdetails/" className="btn btn-sm btn-primary">{newsURL}Read more</a>
+        <h5 className="card-title">{title}... </h5>
+        <p className="card-text">{description}...</p>
+        <a href={newsURL} target= '_blank' className ="btn btn-sm btn-primary">Read more</a>
   </div>
 </div>
 </div>
